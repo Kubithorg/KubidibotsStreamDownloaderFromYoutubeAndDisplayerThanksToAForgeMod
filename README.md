69994c9fc91fcbec8107c40b1cd497b34cb06c75
Burgaud Valentin <devxharos@gmail.com>
2017-10-30 15:25:32 +0100

    Setup forge 1.12.2

--- README.md
+++ README.md
@@ -6,5 +6,22 @@ Kubithon is a charity event for the [telethon]!
 
 Forge mod to replicate the main kubithon stream!
 
+#Setup
+
+Simply clone this repo and run (only for intellij) :
+
+```
+./gradlew setupDecompWorkspace
+and then
+./gradlew genIntellijRuns
+
+```
+In case you will receive an error, follow this [link](http://mcforge.readthedocs.io/en/latest/gettingstarted/) to setup this project.
+
+#Run & build
+
+This mod is a client-side only, you can test your code with `gradlew runClient`.
+Just run `gradlew build` to get the full jar according to this minecraft forge version. 
+
 
 [telethon]: <https://www.afm-telethon.fr/>
--- src/main/java/fr/kubithon/kubidibotsstreamdownloaderfromyoutubeanddisplayerthankstoaforgemod/Streamer.java
+++ src/main/java/fr/kubithon/kubidibotsstreamdownloaderfromyoutubeanddisplayerthankstoaforgemod/Streamer.java
@@ -1,10 +1,15 @@
 package fr.kubithon.kubidibotsstreamdownloaderfromyoutubeanddisplayerthankstoaforgemod;
 
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import org.apache.logging.log4j.Logger;
+
 /**
  * File <b>Streamer</b> located on fr.kubithon.kubidibotsstreamdownloaderfromyoutubeanddisplayerthankstoaforgemod
- * Streamer is a part of KubidibotsStreamDownloaderFromYoutubeAndDisplayerThanksToAForgeMod.
+ * Streamer is a part of kubidibotsstreamdownloaderfromyoutubeanddisplayerthankstoaforgemod.
  * <p>
- * KubidibotsStreamDownloaderFromYoutubeAndDisplayerThanksToAForgeMod is free software: you can redistribute it and/or modify
+ * kubidibotsstreamdownloaderfromyoutubeanddisplayerthankstoaforgemod is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
@@ -20,8 +25,22 @@ package fr.kubithon.kubidibotsstreamdownloaderfromyoutubeanddisplayerthankstoafo
  * Public License for more details (LICENSE file)
  *
  * @author xharos, {@literal <valentin.burgaud@yahoo.fr>}
- * Created the 30/10/17 at 13:31
+ * Created the 30/10/17 at 14:20
  * @since 0.0.1
  */
+@Mod(modid = "streamer", name = "Kubithon - KubidibotsStreamDownloaderFromYoutubeAndDisplayerThanksToAForgeMod", version = "0.0.1", clientSideOnly = true)
 public class Streamer {
+
+    public static Logger logger;
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+        logger = event.getModLog();
+    }
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event) {
+        logger.info("Start mod :)");
+    }
+
 }
