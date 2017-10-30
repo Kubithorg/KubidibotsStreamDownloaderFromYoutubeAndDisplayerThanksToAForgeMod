18636e739ad78652f4820cfbe330a8125a3f8bb9
Burgaud Valentin <devxharos@gmail.com>
2017-10-30 13:32:32 +0100

    First commiy

---
+++ README.md
@@ -0,0 +1,10 @@
+# ![logo](https://kubithon.org/content/img/kubithon.png) Kubithon - KubidibotsStreamDownloaderFromYoutubeAndDisplayerThanksToAForgeMod
+
+Kubithon is a charity event for the [telethon]!
+
+# Description
+
+Forge mod to replicate the main kubithon stream!
+
+
+[telethon]: <https://www.afm-telethon.fr/>
---
+++ src/main/java/fr/kubithon/kubidibotsstreamdownloaderfromyoutubeanddisplayerthankstoaforgemod/Streamer.java
@@ -0,0 +1,27 @@
+package fr.kubithon.kubidibotsstreamdownloaderfromyoutubeanddisplayerthankstoaforgemod;
+
+/**
+ * File <b>Streamer</b> located on fr.kubithon.kubidibotsstreamdownloaderfromyoutubeanddisplayerthankstoaforgemod
+ * Streamer is a part of KubidibotsStreamDownloaderFromYoutubeAndDisplayerThanksToAForgeMod.
+ * <p>
+ * KubidibotsStreamDownloaderFromYoutubeAndDisplayerThanksToAForgeMod is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * <p>
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details (COPYING).
+ * <p>
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ * AssetsBuilder is a free software: See the GNU Lesser General
+ * Public License for more details (LICENSE file)
+ *
+ * @author xharos, {@literal <valentin.burgaud@yahoo.fr>}
+ * Created the 30/10/17 at 13:31
+ * @since 0.0.1
+ */
+public class Streamer {
+}
